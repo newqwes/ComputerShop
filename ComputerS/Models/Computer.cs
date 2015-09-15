6d42f96867e78ee0ce0c63100adff338f159d4b8
newqwes <newnewqwes@gmail.com>
2015-09-15 18:38:52 +0300

    исправление

--- ComputerS/Models/Computer.cs
+++ ComputerS/Models/Computer.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+
+/* В этом файле определен простой класс Computer с автоматически реализуемыми свойствами,
+ которые соответствуют столбцам в созданной таблице базы данных.*/
+
 namespace ComputerS.Models
 {
     public class Computer
     {
-        /* В этом файле определен простой класс Computer с автоматически реализуемыми свойствами,
-         которые соответствуют столбцам в созданной таблице базы данных.*/
         public int ComputerId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
